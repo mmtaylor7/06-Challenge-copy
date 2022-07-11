e1122ae48dcb57f1f0f9a467ba67dd96c22838e7
mmtaylor <taylor.marymargaret@gmail.com>
2022-07-10 22:13:06 -0400

    began displayCurrentWeather function

--- Assets/script.js
+++ Assets/script.js
@@ -5,3 +5,10 @@ var city;
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
 fetch(queryURL);
+
+function displayCurrentWeather() {
+    city = getElementsByClassName(city);
+    if(city) {
+        
+    }
+}
