876e84623a6087228d2d5755322bddcfa0891a68
mmtaylor <taylor.marymargaret@gmail.com>
2022-07-11 00:00:12 -0400

    display data for current day for searched city

--- Assets/script.js
+++ Assets/script.js
@@ -2,15 +2,24 @@ var APIKey = "8059d089214d0c37818b7644ffe96232"
 
 var city;
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
 
-// fetch(queryURL);
-
-function displayCurrentWeather() {
+async function displayCurrentWeather() {
     city = document.querySelector('.user-input').value;
     
     if(city) {
-        
+       let data = await fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=8059d089214d0c37818b7644ffe96232");
+        data = await data.json();
+        let lat = data[0].lat;
+        let lon = data[0].lon;
+        let weatherData = await fetch("https://api.openweathermap.org/data/2.5/weather?lat="+ lat + "&lon=" + lon + "&appid=" + APIKey + "&units=imperial");
+        weatherData = await weatherData.json();
+        document.querySelector(".city").innerHTML = weatherData.name;
+        document.querySelector(".current-temp").innerHTML = weatherData.main.temp;
+        document.querySelector(".current-humidity").innerHTML = weatherData.main.humidity;
+        document.querySelector(".current-wind").innerHTML = weatherData.wind;
+
+        console.log(weatherData);
     }
     
 }
